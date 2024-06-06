ee93503b20a00b7f1942dfaada3696e8ecfbdd4e
Aderemi Abiodun <aderemibiodun1000@gmail.com>
2024-06-06 06:45:22 +0100

    add navbar to component

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+import { IoBagOutline } from "react-icons/io5";
+// import { FiShoppingCart } from "react-icons/fi";
+// <FiShoppingCart />
+const Navbar = () => {
+  return (
+    <nav>
+      <div className="container nav__container">
+        <div className="nav__container-left">
+          <div className="logo">
+            <h2>E-Mall1</h2>
+          </div>
+        </div>
+        <div className="nav__container-right">
+          <div className="cart">
+            <IoBagOutline />
+            <p className="items-count">0</p>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
