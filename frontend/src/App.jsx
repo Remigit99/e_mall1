c4ffe82f3f80ccbef5488523547777ad5eb05aba
Aderemi Abiodun <aderemibiodun1000@gmail.com>
2024-06-06 06:57:04 +0100

    create route for pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,33 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./App.css";
+import Layout from "./components/Layout";
+import Home from "./Pages/Home";
+import Cart from "./Pages/Cart";
 
-import './App.css'
-
-function App() {
-
+const App = () => {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      // errorElement:
+      children: [
+        {
+          index: true,
+          element: <Home />,
+        },
+        {
+          path: "cart",
+          element: <Cart />,
+        },
+      ],
+    },
+  ]);
 
   return (
     <>
-      <h1>My E-mall</h1>
+      <RouterProvider router={router} />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
